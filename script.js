d089264f8e162953b3373b1aeb55258b833cfe9d
Rohitverma12345 <122350697+Rohitverma12345@users.noreply.github.com>
2025-09-27 11:44:40 +0530

    Add full website files

---
+++ script.js
@@ -0,0 +1,17 @@
+// Select all nav links
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault(); 
+
+    const targetId = link.getAttribute("href").substring(1); 
+    const targetSection = document.getElementById(targetId);
+
+
+    targetSection.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+});
